63466c3fc3ec812663d0491f5a8017fe39ac36d3
NarutoUchiha39 <104666748+NarutoUchiha39@users.noreply.github.com>
2024-01-23 23:08:45 +0530

    Update Count.mjs

--- DB_Schema/Count.mjs
+++ DB_Schema/Count.mjs
@@ -3,6 +3,7 @@ export default mongoose.model('Count',new mongoose.Schema({
     date:String,
     in:Number,
     out:Number,
+    busiest_day:String,
     busiest_hour:String,
     student:Number,
     teacher:Number,
